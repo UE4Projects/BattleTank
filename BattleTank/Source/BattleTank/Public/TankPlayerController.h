371f1f776396f190255459ec4d81396d57fad02f
VedBoon <him_jbg@icloud.com>
2017-02-28 16:07:06 -0600

    Fixed Main Menu, Added Fonts, And Set Up a Tank Delegate

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,22 @@
+// Copyright OurReality LLC
+
+#pragma once
+
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // Must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+public:
+	ATank* GetControlledTank() const;
+
+	
+};
